25fe6cf64d7da6d9420dd3c66a75927e491ce36c
Yokichaso <76954140+Yokichaso@users.noreply.github.com>
2021-01-04 23:23:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,6 +23,5 @@ async def Test(ctx):
 
 @bot.command()
 async def YouTubeDL(ctx):
-    await ctx.send('mp4:まだ使えないよ　mp3:http://dl132.y2mate.com/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ1NuYzAveVFRc1N1QmtwNTAvM3VxbE1Ob0JJN2RjaTR5ckhkdHI1VC9NZVkzT1lpbVF1WTB5VjNHRXNvUWJ0QytjdHR3R0M0d2pjQStxbHVlM2szOTFoZ3ErTTVQYkVLQlRZMk4wbkVoaXd6YWF3dlRIcWlxcm9rbUNvRVNoV2hjdXNYVUdMYWFXOUlkWTNIdk9lLzdwMXMxVSszYkd0YzBhaktMUDhoTC8yYmRqNjRndENoY2dKTlpPejhmbXlhUT0%3D')
-
+    await ctx.send('mp3;Yoki.YouTubeDL.com')
 bot.run(token)
