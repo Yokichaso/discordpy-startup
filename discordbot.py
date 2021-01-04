83095e7d63c396b418a5e7a7cd8cc6e36aa89f3b
Yokichaso <76954140+Yokichaso@users.noreply.github.com>
2021-01-04 23:26:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,6 +22,10 @@ async def Test(ctx):
     await ctx.send('確認できました')
 
 @bot.command()
+async def GoogleConnect(ctx):
+    await ctx.send('確認できました')
+
+@bot.command()
 async def YouTubeDL(ctx):
     await ctx.send('mp3;http://dl132.y2mate.com/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ1NuYzAveVFRc1N1QmtwNTAvM3VxbE1Ob0JJN2RjaTR5ckhkdHI1VC9NZVkzT1lpbVF1WTB5VjNHRXNvUWJ0QytjdHR3R0M0d2pjQStxbHVlM2szOTFoZ3ErTTVQYkVLQlRZMk4wbkVoaXd6YWF3dlRIcWlxcm9rbUNvRVNoV2hjdXNYVUdMYWFXOUlkWTNIdk9lLzdwMXMxVSszYkd0YzBhZzZMUDhoTC8yYmRqNjRndENoY2dKTlpPejhmbXlhUT0%3D')
 bot.run(token)
